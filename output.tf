7b1eeb0918e2565827175241e414f1bb1669182b
shubhamlole <loleshubham46@gmail.com>
2026-04-02 12:18:32 +0530

    output file updated successfully

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "Endpoint" {
   description = "internet facing load balancer DNS"
   value = aws_lb.web_lb.dns_name
 }
+output "RdsEndpoint" {
+  description = "rds instance endpoint"
+  value = aws_db_instance.mydb.endpoint
+}
